35a2e1daa81efdb30d47f4c7118758ac75b49e01
Mitch RTX <mitchell@vercidium.com>
2026-07-10 13:17:08 +1000

    README

--- Source/vaudio-unreal/Private/VaRawLog.h
+++ Source/vaudio-unreal/Private/VaRawLog.h
@@ -3,22 +3,26 @@
 #include <cstdio>
 #include <cstdarg>
 
+#include "CoreMinimal.h"
+
 inline void VaRawLog(const wchar_t* Fmt, ...)
 {
-	FILE* F = nullptr;
-	_wfopen_s(&F, L"C:\\vaudio_debug.log", L"a, ccs=UTF-8");
-	if (!F) return;
-
 	va_list Args;
 	va_start(Args, Fmt);
-	vfwprintf(F, Fmt, Args);
+
+	wchar_t Buffer[1024];
+	_vsnwprintf_s(Buffer, _TRUNCATE, Fmt, Args);
 	va_end(Args);
 
-	fwprintf(F, L"\n");
-	fclose(F);
+	FILE* F = nullptr;
+	_wfopen_s(&F, L"C:\\vaudio_debug.log", L"a, ccs=UTF-8");
+	if (F)
+	{
+		fwprintf(F, L"%s\n", Buffer);
+		fclose(F);
+	}
 
-	// TODO - also log to UE editor here?
-	// UE_LOG(LogTemp, Warning, TEXT(Fmt, Args));
+	UE_LOG(LogTemp, Warning, TEXT("%s"), Buffer);
 }
 
 // Matches VALogCallback = void(*)(const char*). Pass directly to vaWorldSetLogCallback,
@@ -28,8 +32,11 @@ inline void VaSdkLogCallback(const char* Message)
 {
 	FILE* F = nullptr;
 	_wfopen_s(&F, L"C:\\vaudio_debug.log", L"a, ccs=UTF-8");
-	if (!F) return;
+	if (F)
+	{
+		fwprintf(F, L"[VA SDK] %hs\n", Message ? Message : "(null)");
+		fclose(F);
+	}
 
-	fwprintf(F, L"[VA SDK] %hs\n", Message ? Message : "(null)");
-	fclose(F);
+	UE_LOG(LogTemp, Warning, TEXT("[VA SDK] %hs"), Message ? Message : "(null)");
 }
